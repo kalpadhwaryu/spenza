da46ea9fcb4b0231e27a56801be1bf76b0232e8a
kalpadhwaryu <kalpadhwaryu1581@gmail.com>
2023-06-03 16:24:02 +0530

    Changed colors of pie chart

--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -20,7 +20,7 @@ const SingleButton = ({option, isSelected, onSelect, height = 40}) => {
       <Text
         style={tw`${
           isSelected ? 'text-white' : 'text-black'
-        } p-0.5 text-center text-sm'}`}>
+        } p-0.5 text-center text-sm`}>
         {option.label}
       </Text>
     </TouchableOpacity>
